75949f858073ad77a562d1582084de6122d5762a
Derek <143875991+GameOver0214@users.noreply.github.com>
2024-09-12 11:48:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if recommended_restaurants[0][0] != "Current restaurant information not found, p
     
     # Creating a dataframe to display recommendations in a table
     recommendations_df = pd.DataFrame(recommended_restaurants, columns=["Restaurant", "Rest Type", "Cuisines", "URL"])
-    recommendations_df = recommendations_df.drop_duplicates()
+    recommendations_df = recommendations_df.drop_duplicates("Restaurant")
     # Make restaurant names clickable links
     recommendations_df['URL'] = recommendations_df.apply(lambda x: f"[{x['Restaurant']}]({x['URL']})", axis=1)
     
