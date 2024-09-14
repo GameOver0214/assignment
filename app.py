1f00db5a3b799f39a8f4da7c8bf533e794d78be3
Derek <143875991+GameOver0214@users.noreply.github.com>
2024-09-14 22:42:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
 # Display the logo at the top of the app
-st.image("assignment/logo.png", width=200)  # Adjust the width as needed
+st.image("logo.jpeg", width=200)  # Adjust the width as needed
 
 # Load the dataset
 df = pd.read_csv('zomato_extracted.csv')
